1f617d42bcbe13cb176b546d8aa533e83ae36578
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-11 04:15:53 -0700

    Assignmentoperations

---
+++ assignmentOperations.js
@@ -0,0 +1,33 @@
+a=20; // here (=) operator assigning value to variable a
+console.log('Assignment=',a);
+
+a=20;
+b=10;
+a+=b;// here (+=) operator adding a and b variable valuse and assigning the sum to a (a+=b == a=a+b)
+console.log('Addition=',a);
+
+a=20;
+b=10;
+a-=b;// here (-=) operator substracting b from a variable valuse and assigning sub to a (a-=b == a=a-b)
+console.log('Substaction=',a);
+
+a=20;
+b=10;
+a/=b;// here (/=) operator dividing a by b variable and assign the result to a (a/=b == a=a/b)
+console.log('Division=',a);
+
+a=20;
+b=10;
+a*=b;// here (*) operator multiplying a and b variable valuse and assign the output to a (a*=b == a=a*b)
+console.log('Multiplication=',a);
+
+a=20;
+b=10;
+a%=b;// here (%) after completing devision of a by b returning reminds value to a (a%=b == a=a%b)
+console.log('Mode=',a);
+
+
+a=2;
+b=3;
+a**=b;// here (**) give the exponent value of a and b and assign to a (a**=b == a=a**b)
+console.log('Exponent=',a);
