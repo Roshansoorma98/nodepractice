38ba50c693841fc9a88170915359e6fb7d0ceb68
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-11 03:12:56 -0700

    airthmaticOperations

---
+++ airthmaticOperations.js
@@ -0,0 +1,40 @@
+a=20; // here (=) operator assigning value to variable a
+console.log(a);
+
+a=20;
+b=10;
+c=a+b;// here (+) operator adding a and b variable valuse
+console.log(c);
+
+a=20;
+b=10;
+c=a-b;// here (-) operator substracting b from a variable valuse
+console.log(c);
+
+a=20;
+b=10;
+c=a/b;// here (/) operator dividing a by b variable
+console.log(c);
+
+a=20;
+b=10;
+c=a*b;// here (*) operator multiplying a and b variable valuse
+console.log(c);
+
+a=20;
+b=10;
+c=a%b;// here (%) returning reminds value to c after completing devision
+console.log(c);
+
+a=20;
+a++;// here (++) operator incrementing the value of a by 1
+console.log(a);
+
+a=20;
+a--;// here (--) here (--) operator dicrementing the value of a by 1
+console.log(a);
+
+a=2;
+b=3;
+c=a**b;// here (**) give the exponent value of a and b
+console.log(c);
