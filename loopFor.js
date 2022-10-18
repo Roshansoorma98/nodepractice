e8c0d303f215beb6592f5dca558f09e388abc0f3
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-18 06:17:39 -0700

    For loop

---
+++ loopFor.js
@@ -0,0 +1,36 @@
+// For loop
+// repetation of the statments
+// travel inside the array
+const a= [5,10,20,30,40];
+for(let i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+
+
+for(let i=0;i<a.length;i++)
+{
+    console.log(a[i]);
+}
+
+
+for(let i=0;i<a.length;i++)
+{
+    a[i]=a[i]*2;
+    
+}
+console.log(a);
+
+
+// For of loop
+
+for(const x of a)
+{
+    console.log(x);
+}
+
+const lan="Roshan"
+for(const x of lan)
+{
+    console.log(x);
+}
