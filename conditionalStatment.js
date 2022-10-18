b221a1227b39d395bf67490f580a1e34ab0bc696
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-18 06:21:54 -0700

    Conditional statment

---
+++ conditionalStatment.js
@@ -0,0 +1,26 @@
+const a=[5,10,20,30,40];
+if(a[1]==10)
+{
+    console.log('True');
+}
+
+
+if(a[0]==20)
+{
+    console.log('condition true');
+}
+else
+{
+    console.log('condition false');
+}
+
+
+if(a[1]==20)
+{
+    console.log('condition true for if block'); 
+}else if(a[1]!=20)
+{
+    console.log('condition true else if block');
+}else{
+    console.log('condition true for else block');
+}
