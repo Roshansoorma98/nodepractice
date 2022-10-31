bcdc686b0081aa0e5ada03fa5cf5e0723e1ffe49
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-31 15:21:53 +0530

    Function

---
+++ function.js
@@ -0,0 +1,23 @@
+function add(a,b)
+{
+ const result= a+b;
+ return result;  
+}
+
+function mul(a,b)
+{
+ const result= a*b;
+ return result;  
+}
+function sub(a,b)
+{
+ const result= a-b;
+ return result;  
+}
+function div(a,b)
+{
+ const result= a/b;
+ return result;  
+}
+
+console.log(mul(2,3));
