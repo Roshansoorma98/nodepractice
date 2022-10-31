e15d6c6ca5d78f1eb0aab170ffab73bb4e82cb98
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-31 15:26:33 +0530

    Import

---
+++ importFile.js
@@ -0,0 +1,11 @@
+/*const factorial1=require('./exportFile');//importing file from exportFile
+const result= factorial1.factorial(5);
+console.log(result);
+
+const prime1=require('./exportFile');//importing file from exportFile
+const result= prime1.prime(20);
+console.log(result);*/
+
+const avg1=require('./exportFile');//importing file from exportFile
+const result= avg1.avg(20,30,50);
+console.log(result);
