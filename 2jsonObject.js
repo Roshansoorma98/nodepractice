2652e12f2b850c34041e9f21f5d08b4a582e4d26
Roshansoorma98 <roshansoorma1998@gmail.com>
2022-10-31 15:30:24 +0530

    Json 2

---
+++ 2jsonObject.js
@@ -0,0 +1,23 @@
+//this is the example of using array in object
+/*const person={name:'Ritik', 
+age:23, 
+city:['Seoni malwa','indore','bhopal'],
+college:'svce',
+address:{block:'Seoni malwa',
+pincode:'461223',
+state:'MP'
+}
+}
+/*for(let key in person)
+{
+    result=person[key];
+    console.log(result);
+}
+
+delete person.city[1];
+
+console.log(person);*/
+
+// using function in object
+
+
